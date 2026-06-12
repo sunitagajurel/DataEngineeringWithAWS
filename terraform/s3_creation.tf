53f6aeb1e6abeec9b08ecabdb0504bf0d8eb8b98
sunitagajurel <sunita.gajurel49@gmail.com>
2026-06-12 16:51:20 -0700

    revomed providers from other code

--- terraform/s3_creation.tf
+++ terraform/s3_creation.tf
@@ -1,14 +1,4 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-west-2"
 }
 
 resource "aws_s3_bucket" "app_bucket" {
